9311448a0238a0e2ab47f6c6f5b8aca53796cd95
aarrgom935 <aarrgom935@g.educaand.es>
2024-01-19 20:40:01 +0100

    .

--- R2D2-POO/src/main/kotlin/Main.kt
+++ R2D2-POO/src/main/kotlin/Main.kt
@@ -1,22 +1,22 @@
 import javax.naming.InitialContext
 
+
+// TODO: Posible mejora usar enum class
 class Robot(val nombre : String){
 
-    /*var posX: Int
-    init {
-        this.posX = 0
-    }
-    Se puede hacer asi cunaod quieres meter codigo dentro, en este caso al asignarle un 0, no tieen sentido*/
-
-    var posX: Int = 0
+    var posX = 0
     var posY = 0
     var dir = 0  // 0 -> PositiveY; 1 -> NegativeX; 2 -> NegativeY; 3 -> PositiveX
 
+    var posXFinal = 0
+    var posYFinal = 0
     /**
      * Mueve el robot de posicion
      * @param movimientos Intarray lista de movimientos
      */
     fun mover(movimientos: IntArray){
+
+
         for (movs in movimientos){
             when (this.dir) {
                 0 -> this.posY += movs
@@ -24,8 +24,17 @@ class Robot(val nombre : String){
                 2 -> this.posY -= movs
                 3 -> this.posX += movs
             }
-            if (this.dir < 3) this.dir +=1 else this.dir = 0
+
+            if (dir == 3) {
+                dir = 0
+            }
+            else{
+                dir++
+            }
         }
+
+
+
     }
 
     fun obtenerDireccion() =
@@ -44,15 +53,18 @@ class Robot(val nombre : String){
 
 }
 
+
+
 fun main(){
     val robot1 = Robot("R2D2")
 
     val movimientos = arrayOf(
-        intArrayOf(10, 2, -2),
+        intArrayOf(10, 5, -2),
         intArrayOf(0, 0, 0),
         intArrayOf(),
         intArrayOf(-10, -5, 2),
-        intArrayOf(-10, -5, 2, 4, -8)
+        intArrayOf(-10, -5, 2, 4, -8),
+        intArrayOf(3,4, 2)
     )
 
     for (movimiento in movimientos){
